282983d75c1b77b37ff87d5528b5d65acff96c2a
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 10:32:08 -0500

    agregando Ejercicio2

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Simples/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("coloca tu calificacion: ");
+            int nota = int.Parse(Console.ReadLine());
+
+            if (nota >= 12)
+            {
+                Console.WriteLine("El estudiante aprobó.");
+            }
+
+
+
+
+        }
+    }
+}
