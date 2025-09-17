7f9603fb3242006ca9fc070691304d081338bf3c
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 11:06:23 -0500

    Agregando Ejercicio 1 a Co-Multiples

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Multiples/Ejercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" coloque un numero [1-3]: ");
+            int dia = int.Parse(Console.ReadLine());
+
+            switch (dia)
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miércoles");
+                    break;
+                default:
+                    Console.WriteLine("Otro día");
+                    break;
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+                
+    }
+}
