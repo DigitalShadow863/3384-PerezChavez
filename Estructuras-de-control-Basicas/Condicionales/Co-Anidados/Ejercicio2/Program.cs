18582d26b00269ae94aa069e0287d8c17ebb17f8
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 11:30:29 -0500

    Agregando Ejercicio2 a Co-Anidados

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Anidados/Ejercicio2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string usuario = "admin";
+            string contraseña = "1234";
+            Console.WriteLine("coloque su usuario: ");
+            usuario = Console.ReadLine();
+            
+
+            if (usuario == "admin")
+            {
+                Console.WriteLine("coloque su contraseña: ");
+                contraseña = Console.ReadLine();
+                if (contraseña == "1234")
+                {
+                    Console.WriteLine("Acceso permitido.");
+                }
+                else
+                {
+                    Console.WriteLine("Contraseña incorrecta.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usuario no válido.");
+            }
+
+
+
+            Console.ReadKey();  
+
+        }
+    }
+}
