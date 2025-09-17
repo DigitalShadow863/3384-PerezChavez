bf38ca7b54c9d40618301d98c8a1da9569caa6a7
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 00:45:40 -0500

    agregando ejercicio1

---
+++ Estructuras-de-control-Basicas/Secuenciales/Ejercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada datos
+            Console.Write("Ingrese el primer número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Proceso
+            int suma = num1 + num2;
+
+            // Salida
+            Console.WriteLine("La suma es: " + suma);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
