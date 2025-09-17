b30010a2d4b603a54e324ab58e0e9268c81a82b2
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 11:29:38 -0500

    Agregando Ejercicio1 a Co-anidados

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Anidados/Ejercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Coloca tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad > 1)
+            {
+                if (edad < 12)
+                {
+                    Console.WriteLine("Es un niño.");
+                }
+                else if (edad < 18)
+                {
+                    Console.WriteLine("Es un adolescente.");
+                }
+                else
+                {
+                    Console.WriteLine("Es un adulto.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("es un bebe");
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
