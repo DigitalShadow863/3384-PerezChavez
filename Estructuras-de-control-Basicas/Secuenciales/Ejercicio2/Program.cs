4a8fb9f5e51e16e56de3d315f4b983b28a7f12d2
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 00:58:06 -0500

    agregando ejercicio2

---
+++ Estructuras-de-control-Basicas/Secuenciales/Ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada datos
+            Console.Write("Ingrese la base del triángulo: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la altura del triángulo: ");
+            double h = double.Parse(Console.ReadLine());
+
+            // Proceso
+            double area = (b * h) / 2;
+
+            // Salida
+            Console.WriteLine("El área del triángulo es: " + area);
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
