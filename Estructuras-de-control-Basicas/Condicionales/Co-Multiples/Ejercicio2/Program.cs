2931f4089d45d85a12c2690d82ae7e49989a75d3
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 11:08:06 -0500

    Agregando Ejercicio 2 a Co-Multiples

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Multiples/Ejercicio2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elija una opción [1-4]: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("has elegido sumar. "); break;
+                case 2:
+                    Console.WriteLine("has elegido restar. "); break;
+                case 3:
+                    Console.WriteLine("has elegido multiplicar. "); break;
+                case 4:
+                    Console.WriteLine("has elegido dividir. "); break;
+                default:
+                    Console.WriteLine("Opción no válida"); break;
+            }
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
