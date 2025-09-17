eb3c375f8bdb3cbf2d424823c6d6e1007b544bed
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 10:41:24 -0500

    Agregando Ejercicio2

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Dobles/Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("coloque su edad: ");
+            int edad = int.Parse(Console.ReadLine());   
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Es menor de edad.");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
