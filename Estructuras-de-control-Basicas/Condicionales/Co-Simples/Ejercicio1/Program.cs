2ed099924eafcd6456741a32b8c986673edb4c0e
DigitalShadow863 <N00469831@upn.pe>
2025-09-17 10:30:39 -0500

    agregando Ejercicio1

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Simples/Ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("coloca el numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            if (numero > 0)
+            {
+                Console.WriteLine("El número es positivo.");
+            }
+
+
+
+            Console.ReadKey();  
+
+
+
+        }
+    }
+}
