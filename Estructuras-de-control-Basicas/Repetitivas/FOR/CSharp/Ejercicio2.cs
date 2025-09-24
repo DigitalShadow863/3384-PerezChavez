42f2829bbbcecf16d7225a3379a794695bb59ea0
DigitalShadow863 <N00469831@upn.pe>
2025-09-23 22:13:56 -0500

    agregando ejercicios en CSharp a FOR

---
+++ Estructuras-de-control-Basicas/Repetitivas/FOR/CSharp/Ejercicio2.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int suma = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            suma += i; // suma = suma + i
+        }
+
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
