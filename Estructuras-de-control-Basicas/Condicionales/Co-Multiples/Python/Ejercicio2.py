e3cc85afeff408075223e842e8cfec1436713178
DigitalShadow863 <N00469831@upn.pe>
2025-09-23 21:33:26 -0500

    agregando ejemplos python a co-multiples

---
+++ Estructuras-de-control-Basicas/Condicionales/Co-Multiples/Python/Ejercicio2.py
@@ -0,0 +1,9 @@
+nota = float(input("Ingrese la nota: "))
+if nota >= 18:
+    print("Calificación: A")
+elif nota >= 15:
+    print("Calificación: B")
+elif nota >= 12.5:
+    print("Calificación: C")
+else:
+    print("Calificación: D")
